a5200a26b9020e925c000f670590699dbfe87e26
Alexis Serrano <alexisfserrano@outlook.com>
2019-06-14 17:53:38 -0300

    *Servicio *assets * cards

--- angularHeroes/src/app/app-routing.module.ts
+++ angularHeroes/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicio', component: HomeComponent },
+  { path: 'acercaDe', component: AboutComponent },
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'heroe/:id', component: HeroeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- angularHeroes/src/app/components/heroes/heroes.component.ts
+++ angularHeroes/src/app/components/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroeService } from '../../services/heroe.service';
 
 @Component({
   selector: 'app-heroes',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  constructor(public _infoHeroes: HeroeService) { }
 
   ngOnInit() {
   }
