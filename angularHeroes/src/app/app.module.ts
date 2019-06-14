cecf1a930c9cac29bb214e871740b3c6259c0bdc
Alexis Serrano <alexisfserrano@outlook.com>
2019-06-14 16:34:16 -0300

    Se crearon los componentes.

--- angularHeroes/src/app/app.module.ts
+++ angularHeroes/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
+import { SearchComponent } from './components/search/search.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent,
+    HeroeComponent,
+    SearchComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
